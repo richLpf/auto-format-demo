c4ceed249167caf444c0eb8e54965f3232302a4b
richLpf <lpengfei66@163.com>
2022-03-14 17:29:49 +0800

    ci: change config

--- src/index.js
+++ src/index.js
@@ -2,11 +2,11 @@ var a = 'This is a case in point'
 
 function test() {
   let b = 1
-    let c = 3
+  let c = 3
       let d = 5
-     return a + b + c + d
+        return a + b + c + d
 }
 
 test()
 
-console.log("a", a)
+console.log('a', a)
